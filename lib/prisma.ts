6530ae9d28ca09f9aca5d5c9da397790776e9e82
Biplob Talukdar <biplob.codes@gmail.com>
2026-06-03 15:46:32 +0600

    Fix the issue woth prisma with build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
